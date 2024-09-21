78733a8494f07e654b5d0871fd69508ad7e67286
zebin huang <1667535930@qq.com>
2024-09-21 15:25:17 +0800

    完成确认注册的api，修改了两个api绑定的url

---
+++ src/main/java/com/example/math/dto/LoginResponse.java
@@ -0,0 +1,21 @@
+package com.example.math.dto;
+
+// 登录响应的数据结构
+public class LoginResponse {
+    private final boolean success;
+    private final String message;
+
+    public LoginResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
